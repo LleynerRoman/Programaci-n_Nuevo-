2ccee2276c64375768b0653f6893fed38c36d98d
unknown <lleynerromanpe@gmail.com>
2024-10-18 09:54:13 -0500

    Pagina WEB

---
+++ semana1.R
@@ -0,0 +1,37 @@
+notas<-c(15,16,18,19,15) |>
+  mean()
+library(tidyverse)
+notas<-c(15,17,18,16,14) %>% +1 %>% 
+  mean()
+
+notas<-c(15,17,18,16,14) %>% +1 %>% 
+  mean()
+notas<-c(15,16,18,19,15|>
+           mean()
+           
+
+# importacion de datos ----------------------------------------------------
+
+          
+
+# importar csv ------------------------------------------------------------
+
+         
+tsvdt <- read.delim("Date/LA MOLINA 2014 POTATO WUE (FB) - fb.tsv")
+
+# importar csv ------------------------------------------------------------
+
+csvdt <- read.csv(file = "Date/LA MOLINA 2014 POTATO WUE (FB) - fb.csv")
+library(openxls)
+dtxls <- read.
+install.packages("openxlsx")
+library("openxlsx")
+dtxla <- read.xlsx("Date/LA MOLINA 2014 POTATO WUE (FB).xlsx"
+                   , sheet = "fb")
+library("inti")
+library("googlesheets4") 
+url("https://docs.google.com/spreadsheets/d/15r7ZwcZZHbEgltlF6gSFvCTFA-CFzVBWwg3mFlRyKPs/edit?gid=172957346#gid=172957346")
+
+url <- "https://docs.google.com/spreadsheets/d/15r7ZwcZZHbEgltlF6gSFvCTFA-CFzVBWwg3mFlRyKPs/edit?gid=172957346#gid=172957346"
+gs <- as_sheets_id(url)
+fb <- goo
